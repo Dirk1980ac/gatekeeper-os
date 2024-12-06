90eb26def62aa801793aae6c1b8bc3abf0f8cb8c
Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
2024-12-06 19:50:05 +0100

    Add usr/.

---
+++ usr/local/bin/device-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0
+
+if [ ! -f /etc/yggdrasil.conf ]; then
+	# Configure yggdrasil
+	# This has to be done on first boot since the private/public key have to be
+	# generated for each host.
+	/usr/bin/yggdrasil -genconf -json >/etc/yggdrasil.generated.conf
+	jq '.Peers = ["tls://ygg.yt:443","tls://ygg.mkg20001.io:443","tls://vpn.ltha.de:443", "tls://ygg-uplink.thingylabs.io:443", "tls://supergay.network:443","tls://[2a03:3b40:fe:ab::1]:993","tls://37.205.14.171:993"]' /etc/yggdrasil.generated.conf >/etc/yggdrasil.conf
+fi
